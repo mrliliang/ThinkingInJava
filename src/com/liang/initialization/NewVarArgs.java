cd1c8c5ef6e3c8abf978b7260b482d11b7dae3c6
liliang <llzgz@qq.com>
2018-03-23 10:27:45 +0800

    first commit.

---
+++ src/com/liang/initialization/NewVarArgs.java
@@ -0,0 +1,17 @@
+package com.liang.initialization;
+
+public class NewVarArgs {
+    static void printArray(Object... args) {
+        for (Object obj : args)
+            System.out.println(obj + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printArray(new Integer(47), new Float(3.14), new Double(11.11));
+        printArray("one", "two", "three");
+
+        printArray((Object[])new Integer[]{1, 2, 3, 4});
+        printArray();
+    }
+}
