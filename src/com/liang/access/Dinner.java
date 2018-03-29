b62d6af98fb76d2a499c3961ca9b9ad2f66cfafb
liliang <llzgz@qq.com>
2018-03-29 10:30:16 +0800

    first commit.

---
+++ src/com/liang/access/Dinner.java
@@ -0,0 +1,10 @@
+package com.liang.access;
+
+import com.liang.access.dessert.Cookie;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+
+    }
+}
---
+++ src/com/liang/access/Pie.java
@@ -0,0 +1,7 @@
+package com.liang.access;
+
+public class Pie {
+    void f() {
+        System.out.println("Pie.f()");
+    }
+}
