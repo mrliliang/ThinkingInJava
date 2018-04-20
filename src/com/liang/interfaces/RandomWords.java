0c2591da979712b23dd0ac75671d7c58ec077b16
liliang <llzgz@qq.com>
2018-04-20 10:29:19 +0800

    first commit.

---
+++ src/com/liang/interfaces/RandomWords.java
@@ -0,0 +1,46 @@
+package com.liang.interfaces;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomWords implements Readable {
+
+    private static Random rand = new Random(47);
+    private static final char[] CAPITALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static final char[] LOWERS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final char[] VOWELS = "aeiou".toCharArray();
+
+    private int count;
+
+    public RandomWords(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public int read(CharBuffer cb) throws IOException {
+        if (count-- == 0) {
+            return -1;
+        }
+
+        cb.append(CAPITALS[rand.nextInt(CAPITALS.length)]);
+        for (int i = 0; i < 4; i++) {
+            cb.append(VOWELS[rand.nextInt(VOWELS.length)]);
+            cb.append(LOWERS[rand.nextInt(LOWERS.length)]);
+        }
+        cb.append(" ");
+        return 10;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(new RandomWords(10));
+        while (s.hasNext()) {
+            System.out.println(s.next());
+        }
+        s = new Scanner(System.in);
+        while (s.hasNext()) {
+            System.out.println(s.next());
+        }
+    }
+}
