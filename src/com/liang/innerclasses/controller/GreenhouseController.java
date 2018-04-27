18ea424bbac9fd34a15884b18f87ff8db46e0b65
liliang <llzgz@qq.com>
2018-04-27 13:34:17 +0800

    first commit.

---
+++ src/com/liang/innerclasses/controller/GreenhouseController.java
@@ -0,0 +1,24 @@
+package com.liang.innerclasses.controller;
+
+public class GreenhouseController {
+    public static void main(String[] args) {
+        GreenhouseContorls gc = new GreenhouseContorls();
+        gc.addEvent(gc.new Bell(900));
+
+        Event[] eventList = {
+                gc.new ThermostatNight(0),
+                gc.new LightOn(200),
+                gc.new LightOff(400),
+                gc.new WaterOn(600),
+                gc.new WaterOfff(800),
+                gc.new ThermostatDay(1400)
+        };
+
+        gc.addEvent(gc.new Restart(2000, eventList));
+        if (args.length == 1) {
+            gc.addEvent(new GreenhouseContorls.Terminate(new Integer(args[0])));
+        }
+
+        gc.run();
+    }
+}
