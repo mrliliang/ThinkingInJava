a3cca2f46780d918b605252874f6a963e1b2360f
liliang <llzgz@qq.com>
2018-05-07 13:35:22 +0800

    first commit.

---
+++ src/com/liang/holding/CollectionSequence.java
@@ -0,0 +1,16 @@
+package com.liang.holding;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class CollectionSequence extends AbstractCollection {
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/com/liang/holding/MapOfList.java
@@ -0,0 +1,43 @@
+package com.liang.holding;
+
+import sun.dc.path.PathError;
+import typeinfo.interfacea.A;
+import typeinfo.pets.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.liang.util.Print.print;
+
+public class MapOfList {
+    public static Map<Person, List<? extends Pet>> petPeople = new HashMap<>();
+    static {
+        petPeople.put(new Person("Dawn"), Arrays.asList(new Cymric("Molly"),
+                new Mutt("Spot")));
+        petPeople.put(new Person("Kate"), Arrays.asList(new Cat("Shackleton"),
+                new Cat("Elsie May"), new Dog("Margrett")));
+        petPeople.put(new Person("Marilyn"),
+                Arrays.asList(
+                        new Pug("Louie aka Louis Snorkelstein " + "Dpree"),
+                        new Cat("Standford aka Stinky el Negro"),
+                        new Cat("Pinkola")));
+        petPeople.put(new Person("Luke"),
+                Arrays.asList(new Rat("Fuzzy"), new Rat("Fizzy")));
+        petPeople.put(new Person("Isaac"),
+                Arrays.asList(new Rat("Freckly")));
+
+    }
+
+    public static void main(String[] args) {
+        print("People: " + petPeople.keySet());
+        print("Pets: " + petPeople.values());
+        for (Person person : petPeople.keySet()) {
+            print(person + " has: ");
+            for (Pet pet : petPeople.get(person)) {
+                print("    " + pet);
+            }
+        }
+    }
+}
