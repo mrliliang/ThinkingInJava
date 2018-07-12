d8d6a43672de646a4757639ea417c4441023e383
liliang <llzgz@qq.com>
2018-07-12 14:45:08 +0800

    generic

---
+++ src/com/liang/generics/FilledListMaker.java
@@ -0,0 +1,4 @@
+package com.liang.generics;
+
+public class FilledListMaker {
+}
---
+++ src/com/liang/generics/ReturnGenericType.java
@@ -0,0 +1,13 @@
+package com.liang.generics;
+
+public class ReturnGenericType<T extends HasF> {
+    private T obj;
+
+    public ReturnGenericType(T x) {
+        obj = x;
+    }
+
+    public T get() {
+        return obj;
+    }
+}
