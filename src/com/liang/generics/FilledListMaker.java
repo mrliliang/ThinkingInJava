ef1035126ff3cf50d0c404fbfb22ab6577ac2d4a
liliang <llzgz@qq.com>
2018-07-16 13:36:41 +0800

    generics

--- src/com/liang/generics/FilledListMaker.java
+++ src/com/liang/generics/FilledListMaker.java
@@ -1,4 +1,20 @@
 package com.liang.generics;
 
-public class FilledListMaker {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilledListMaker<T> {
+    List<T> create(T t, int n) {
+        List<T> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            result.add(t);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FilledListMaker<String> stringMaker = new FilledListMaker<>();
+        List<String> list = stringMaker.create("Hello", 4);
+        System.out.println(list);
+    }
 }
