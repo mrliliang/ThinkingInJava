c51ced47abdbbbfa431eee5032777052322813c1
liliang <llzgz@qq.com>
2018-07-17 13:33:02 +0800

    generics

---
+++ src/com/liang/generics/ArrayOfGenericReference.java
@@ -0,0 +1,8 @@
+package com.liang.generics;
+
+
+class Generic<T> {}
+
+public class ArrayOfGenericReference {
+    static Generic<Integer>[] gia;
+}
---
+++ src/com/liang/generics/ListOfGenerics.java
@@ -0,0 +1,16 @@
+package com.liang.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfGenerics<T> {
+    private List<T> array = new ArrayList<>();
+
+    public void add(T item) {
+        array.add(item);
+    }
+
+    public T get(int index) {
+        return array.get(index);
+    }
+}
